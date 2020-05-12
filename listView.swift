35af4a535709c003b0cf7c73fe52a3c25b19771e
Teo-WSIB <teo@wsib-innovation.ca>
2020-05-12 14:44:02 -0400

    Changed WatchData struct to class for accessing methods. Changed Anys to WatchData array types

--- listView.swift
+++ listView.swift
@@ -1,11 +1,3 @@
-//
-//  listView.swift
-//  testMVP WatchKit Extension
-//
-//  Created by Bilal Malik on 2020-05-11.
-//  Copyright © 2020 Bilal Malik. All rights reserved.
-//
-
 import SwiftUI
 
 struct listView: View {
@@ -14,14 +6,18 @@ struct listView: View {
     var body: some View {
         VStack {
             List(0 ..< ticketList.count) { result in
-                //Text("\(result.address)")
-                Text("Rosewell and Fairway Street")
-                Text("Haven't Responded").foregroundColor(Color.red)
+                Text(self.ticketList[result].currentTime)
+                Text(self.ticketList[result].address)
+                if(self.ticketList[result].isResponded){
+                    Text("Responded").foregroundColor(Color.green)
+                }
+                else{
+                    Text("Haven't Responded").foregroundColor(Color.red)
+                }
             }
         }
     }
 }
-
 struct listView_Previews: PreviewProvider {
     static var previews: some View {
         listView(ticketList : [watchData]())
