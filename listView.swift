bb114ba82db9eff5b625844212a5db68475eb613
bilal-wsib <bilal@wsib-innovation.ca>
2020-05-13 12:44:41 -0400

    additional screen to respond to user ticket

--- listView.swift
+++ listView.swift
@@ -1,23 +1,29 @@
 import SwiftUI
 
 struct listView: View {
-    var ticketList = [watchData]()
+    @State var ticketList = [watchData]()
     
     var body: some View {
         VStack {
             List(0 ..< ticketList.count) { result in
-                Text(self.ticketList[result].currentTime)
-                Text(self.ticketList[result].address)
-                if(self.ticketList[result].isResponded){
-                    Text("Responded").foregroundColor(Color.green)
+                NavigationLink(destination: responseDecision(instance: self.ticketList, index: result)){
+                    VStack{
+                        Text(self.ticketList[result].currentTime)
+                        Text(self.ticketList[result].address)
+                    
+                    if(self.ticketList[result].isResponded){
+                        Text("Responded").foregroundColor(Color.green)
+                    }
+                    else{
+                        Text("Haven't Responded").foregroundColor(Color.red)
+                    }
                 }
-                else{
-                    Text("Haven't Responded").foregroundColor(Color.red)
                 }
             }
         }
     }
 }
+
 struct listView_Previews: PreviewProvider {
     static var previews: some View {
         listView(ticketList : [watchData]())
--- responseDecision.swift
+++ responseDecision.swift
@@ -9,13 +9,74 @@
 import SwiftUI
 
 struct responseDecision: View {
+    @State var instance = [watchData]()
+    var index = Int()
+    @State var lock : Int = 0
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack{
+            
+                Button(action: {
+                    self.instance[self.index].isResponded=true
+                    self.lock=1
+                    print(self.instance[self.index].isResponded)
+                }) {
+                    HStack{
+                    
+                         VStack {
+                            Text("\(self.instance[self.index].address)")
+                            Text("Send Help!")
+                         }
+                         if(lock==1){
+                            Image(systemName: "checkmark.shield.fill")
+                         }
+                     }
+                }
+            
+            
+            Button(action: {
+                self.instance[self.index].isResponded=true
+                self.lock=2
+                print(self.instance[self.index].isResponded)
+            }) {
+                HStack{
+               
+                    VStack {
+                        Text("I'm OK!")
+                    }
+                    if(lock==2){
+                        Image(systemName: "checkmark.shield.fill")
+                    }
+                }
+            }
+            
+            Button(action: {
+                self.instance[self.index].isResponded=true
+                self.lock=3
+                print(self.instance[self.index].isResponded)
+            }) {
+                HStack{
+               
+                    VStack {
+                        Text("False Alarm")
+                    }
+                    if(lock==3){
+                        Image(systemName: "checkmark.shield.fill")
+                    }
+                }
+            }
+            
+            NavigationLink(destination: listView(ticketList: self.instance)){
+                Image(systemName: "paperplane.fill")
+            }
+        }
     }
 }
 
+
 struct responseDecision_Previews: PreviewProvider {
     static var previews: some View {
-        responseDecision()
+        responseDecision(instance: [watchData](),index: Int())
     }
 }
+
